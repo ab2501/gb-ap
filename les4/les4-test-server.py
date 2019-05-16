bccea731df104a6bfb35d6c2a7a802b84a3139b3
ab2501 <43670199+ab2501@users.noreply.github.com>
2019-05-16 11:17:39 +0300

    Create les4-test-server.py

---
+++ les4/les4-test-server.py
@@ -0,0 +1,50 @@
+import unittest
+import lesson3_server
+import subprocess
+
+
+
+class Test0Parsing(unittest.TestCase):
+    def testNumberParameters(self):
+        self.assertEqual(len(lesson3_server.parsing()), 3, 'Doesn''t contain three parameters needed to start')
+        print("test 1")
+        pass
+
+    def testParametersAreNotNone(self):
+        self.assertNotEqual(lesson3_server.parsing()[0], None, 'IP address could not be None or Null')
+        self.assertNotEqual(lesson3_server.parsing()[1], None, 'Port number could not be None or Null')
+        print("test 2")
+        pass
+
+
+class TestConnect(unittest.TestCase):
+    def testDataAreNotNone(self):
+        print("test 3")
+        params = lesson3_server.parsing()
+        params[2] = False
+        #runresult = lesson3_server.myserverup(params)
+        #print(params[0])
+        #print(type(runresult)) #return NoneType somehow
+        #self.assertNotEqual(runresult, None, 'data could not be None or Null')
+
+        def run_command(command):
+            p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,  shell=True)
+            #args = [command, ""]
+            #process = subprocess.Popen(args)
+
+            #data = process.communicate()
+            #return iter(p.stdout.readline, b'')
+            #return p.stdout
+            p.kill()
+            return p
+        runresult = run_command(lesson3_server.myserverup(params))
+        #runresult = (lesson3_server.myserverup(params))
+        print(type(runresult))
+        pass
+        # return None
+
+
+# для завершения теста
+# нужно запустить или скрипт клиента, или файл теста клиента.
+if __name__ == '__main__':
+    unittest.main()
