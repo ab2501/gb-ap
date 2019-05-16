90f4f2b9d9ede98a916e86748425fe9a8556418f
ab2501 <43670199+ab2501@users.noreply.github.com>
2019-05-16 11:21:26 +0300

    Update les4-test-client.py

--- les4/les4-test-client.py
+++ les4/les4-test-client.py
@@ -1,17 +1,17 @@
 import unittest
-import lesson3_client
+import client
 
 
 
 class TestParsing(unittest.TestCase):
     def testNumberParameters(self):
-        self.assertEqual(len(lesson3_client.parsing()), 2, 'Doesn''t contain two parameters needed to start')
+        self.assertEqual(len(client.parsing()), 2, 'Doesn''t contain two parameters needed to start')
         print("test 1")
         pass
 
     def testParametersAreNotNone(self):
-        self.assertNotEqual(lesson3_client.parsing()[0], None, 'IP address could not be None or Null')
-        self.assertNotEqual(lesson3_client.parsing()[1], None, 'Port number could not be None or Null')
+        self.assertNotEqual(client.parsing()[0], None, 'IP address could not be None or Null')
+        self.assertNotEqual(client.parsing()[1], None, 'Port number could not be None or Null')
         print("test 2")
         pass
 
@@ -19,8 +19,8 @@ class TestParsing(unittest.TestCase):
 class TestConnect(unittest.TestCase):
     def testDataAreNotNone(self):
         print("test 3")
-        params = lesson3_client.parsing()
-        runresult = lesson3_client.myconnect(params)
+        params = client.parsing()
+        runresult = client.myconnect(params)
         self.assertNotEqual(runresult, None, 'data could not be None or Null')
         pass
 
